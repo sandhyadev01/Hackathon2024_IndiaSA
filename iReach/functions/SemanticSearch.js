27f8d2d982a182f9a854b2446f40ed30ac343995
Ajay Raghav <ajay.raghav@hotmail.com>
2024-02-29 17:50:10 +0530

    Changed app name

---
+++ iReach/functions/SemanticSearch.js
@@ -0,0 +1,76 @@
+async function getEmbedding(query) {
+  const key = context.values.get("OpenAI-Key");
+  try {
+    const url = 'https://api.openai.com/v1/embeddings';
+
+    let response = await context.http.post({
+      url: url,
+      headers: {
+        'Authorization': [`Bearer ${key}`],
+        'Content-Type': ['application/json']
+      },
+      body: JSON.stringify({
+        input: query,
+        model: "text-embedding-ada-002"
+      })
+    });
+    
+    // console.error(query);
+    // console.error(key);
+    
+    // return response.body.text();
+
+    return EJSON.parse(response.body.text()).data[0].embedding;
+
+  }
+  catch (e) {
+    console.error(e);
+  }
+}
+
+async function findSimilarDocuments(embedding) {
+  const mongodb = context.services.get('mongodb-atlas');
+  const db = mongodb.db('ireach');
+  const collection = db.collection('prospects');
+
+  try {
+    const documents = await collection.aggregate([
+      {
+        "$vectorSearch": {
+          "index": "vector_index",
+          "queryVector": embedding,
+          "path": "UseCaseDescription_embeddings",
+          "numCandidates": 5,
+          "limit": 5
+        }
+      },
+      {
+        "$project": {
+          UseCaseDescription: 1,
+          AccountName: 1,
+          Industry:1
+        }
+      }
+    ]).toArray();
+
+    return documents;
+  }
+  catch (e) {
+    console.error(embedding + e);
+    // console.error(e);
+  }
+}
+
+exports = async function ({ query, headers, body }, response) {
+
+  try {
+
+    const embedding = await getEmbedding(query.s);
+    const documents = await findSimilarDocuments(embedding);
+
+    return { results: documents };
+  } catch (err) {
+    console.error(err);
+    return [err];
+  }
+};
--- iReach/functions/createEmbedding.js
+++ iReach/functions/createEmbedding.js
@@ -1,64 +1,58 @@
-exports = async function () {
+exports = async function(changeEvent) {
+    // Get the full document from the change event.
+    const doc = changeEvent.fullDocument;
+
+    // Define the OpenAI API url and key.
+    const url = 'https://api.openai.com/v1/embeddings';
+    // Use the name you gave the value of your API key in the "Values" utility inside of App Services
     const openai_key = context.values.get("openAI_value");
-  
-    // Get the cluster in MongoDB Atlas.
-    const mongodb = context.services.get('mongodb-atlas');
-    const db = mongodb.db('Embeddings_database'); // Replace with your database name.
-    const collection = db.collection('prospects'); // Replace with your collection name.
-  
     try {
-      const pendingDocs = await collection.aggregate([{ $match: { new_embedding: { $exists: false }, UseCaseIndustryCombined: { $exists: true } } }]).toArray();
-  
-      pendingDocs.forEach(async doc => {
+        console.log(`Processing document with id: ${doc._id}`);
+
         // Call OpenAI API to get the embeddings.
-        const url = 'https://api.openai.com/v1/embeddings';
-  
         let response = await context.http.post({
-          url: url,
-          headers: {
-            'Authorization': [`Bearer ${openai_key}`],
-            'Content-Type': ['application/json']
-          },
-          body: JSON.stringify({
-            // The field inside your document that contains the data to embed, here it is the “plot” field from the sample movie data.
-            input: doc.UseCaseIndustryCombined,
-            model: "text-embedding-ada-002"
-          })
+            url: url,
+             headers: {
+                'Authorization': [`Bearer ${openai_key}`],
+                'Content-Type': ['application/json']
+            },
+            body: JSON.stringify({
+                // The field inside your document that contains the data to embed, here it is the "plot" field from the sample movie data.
+                input: doc.UseCaseIndustryCombined,
+                model: "text-embedding-ada-002"
+            })
         });
-  
+
         // Parse the JSON response
         let responseData = EJSON.parse(response.body.text());
-  
+
         // Check the response status.
-        if (response.statusCode === 200) {
-          console.log("Successfully received embedding.");
-  
-          const embedding = responseData.data[0].embedding;
-  
-          // Update the document in MongoDB.
-          const result = await collection.updateOne(
-            { _id: doc._id },
-            // The name of the new field you’d like to contain your embeddings.
-            { $set: { new_embedding: embedding } }
-          );
-  
-          if (result.modifiedCount === 1) {
-            console.log("Successfully updated the document.");
-          } else {
-            console.log("Failed to update the document.");
-          }
+        if(response.statusCode === 200) {
+            console.log("Successfully received embedding.");
+
+            const embedding = responseData.data[0].embedding;
+
+            // Use the name of your MongoDB Atlas Cluster
+            const collection = context.services.get("mongodb-atlas").db("Embeddings_database").collection("prospects");
+
+            // Update the document in MongoDB.
+            const result = await collection.updateOne(
+                { _id: doc._id },
+                // The name of the new field you'd like to contain your embeddings.
+                { $set: { plot_embedding: embedding }}
+            );
+
+            if(result.modifiedCount === 1) {
+                console.log("Successfully updated the document.");
+            } else {
+                console.log("Failed to update the document.");
+            }
         } else {
-          console.log(`Failed to receive embedding. Status code: ${response.statusCode}`);
-          
-          await collection.updateOne(
-            { _id: doc._id },
-            // The name of the new field you’d like to contain your embeddings.
-            { $set: { new_embedding: "failed" } }
-          );
+            console.log(`Failed to receive embedding. Status code: ${JSON.stringify(response)}`);
         }
-  
-      });
-    } catch (err) {
-      console.error(err);
+
+    } catch(err) {
+        console.error(err);
     }
-  };
+};
+  
