b3f89c4b27307b3c3b4ab421d5be58c1d0256cae
Ajay Raghav <ajay.raghav@hotmail.com>
2024-03-01 12:30:02 +0530

    BaaS Changes

--- iReach/functions/Search_Autocomplete.js
+++ iReach/functions/Search_Autocomplete.js
@@ -1,47 +1,52 @@
 async function findDocuments(q) {
-  const mongodb = context.services.get('mongodb-atlas');
-  const db = mongodb.db('ireach');
-  const collection = db.collection('prospects');
-
-  try {
-    const documents = await collection.aggregate([
+    const mongodb = context.services.get('mongodb-atlas');
+    const db = mongodb.db('ireach');
+    const collection = db.collection('prospects');
+  
+    try {
+      const documents = await collection.aggregate([
         {
             $search: {
               index: "autocomplete_prospects",
-              autocomplete:{
+              autocomplete: {
                 query: q,
-                path:"AccountName",
-                 'fuzzy': { "maxEdits": 1, "prefixLength": 2 }
-                }
-              }
+                path: "AccountName",
+                fuzzy: {
+                  maxEdits: 1,
+                  prefixLength: 2,
+                },
+              },
+            },
           },
-      {
-        "$limit": 5
-      },
-      {
-        "$project": {
+        {
+          "$project": {
+          UseCaseDescription: 1,
           AccountName: 1,
-          "_id": 0
- //         score: { $meta: 'searchScore' }
+          Industry: 1,
+          AnnualRunRate: 1,
+          AccountId: 1,
+            "_id": 0
+   //         score: { $meta: 'searchScore' }
+          }
         }
-      }
-    ]).toArray();
-    return documents;
+      ]).toArray();
+  
+      return documents;
+    }
+    catch (e) {
+      console.error(e);
+    }
   }
-  catch (e) {
-    console.error(e);
-  }
-}
-
-exports = async function ({ query, headers, body }, response) {
-
-  try {
-   
-    const documents = await findDocuments(query.s);
-
-    return { results: documents };
-  } catch (err) {
-    console.error(err);
-    return [err];
-  }
-};
+  
+  exports = async function ({ query, headers, body }, response) {
+  
+    try {
+     
+      const documents = await findDocuments(query.s);
+  
+      return { results: documents };
+    } catch (err) {
+      console.error(err);
+      return [err];
+    }
+  };
