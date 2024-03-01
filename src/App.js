3334f73b0dc41a3447253feb04cbb86927662a7c
Ajay Raghav <ajay.raghav@hotmail.com>
2024-03-01 17:30:30 +0530

    generate email integration completed

--- src/App.js
+++ src/App.js
@@ -24,19 +24,17 @@ const IReachApp = () => {
   };
 
   const user = {
-    username: 'John Doe',
-    email: 'john@example.com'
+    username: 'Smith John',
+    email: 'smith.john@mongodb.com'
   };
 
   const [accounts, setAccounts] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const [showOptions, setShowOptions] = useState(false);
   const [promptText, setPromptText] = useState("");
   const [emailText, setEmailText] = useState("");
   const [selectedAccount, setSelectedAccount] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [autocompleteTimeout, setAutocompleteTimeout] = useState(0);
-  const [isPromtTextBarVisible, setIsPromptTextBarVisible] = useState(false);
 
 
   const handleChange = (e) => {
@@ -58,10 +56,12 @@ const IReachApp = () => {
     setPromptText(e.target.value);
   };
 
+  const handleEmailChange = (e) => {
+    setEmailText(e.target.value);
+  };
+
   const handleSelect = (accountId) => {
     setSelectedAccount(accountId);
-    setIsPromptTextBarVisible(true);
-
   }
 
   const handleSubmit = (e, mode) => {
@@ -107,16 +107,15 @@ const IReachApp = () => {
 
 
   async function fetchGeneratedEmail(promptText) {
-
     try {
       const response = await fetch(
-        `https://ap-south-1.aws.data.mongodb-api.com/app/ireach-dodfh/endpoint/generateEmail?accountId=${selectedAccount.AccountId}&prompt=${promptText}`
+        `https://ap-south-1.aws.data.mongodb-api.com/app/ireach-dodfh/endpoint/generateEmail?accountId=${selectedAccount}&prompt=${promptText}`
       );
 
       const emailText = (await response.json()).results;
 
       if (emailText) {
-        return ({ results: emailText, success: true });
+        return ({ result: emailText, success: true });
       }
       else {
         emailText = 'Try again with a different search query';
@@ -153,6 +152,7 @@ const IReachApp = () => {
 
   const getEmailText = async (promptText) => {
     const { result, success } = await fetchGeneratedEmail(promptText);
+
     if (success) {
       setEmailText(result);
     }
@@ -163,89 +163,96 @@ const IReachApp = () => {
   }
 
   return (
-    <div>
-      <header className='header'>
-        <div style={{ marginLeft: '10px' }}>
-          <FiHome size="24px" color='black' />
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', color: 'black' }}>
-          <FiUser size="30px" color='black' marginRight='15px' />
+    <div className="motherOfAll">
+      <div className='mainForm'>
+        {accounts.length === 0 && <img className='bg-image' src={"logo_large.png"} />}
+        <form>
+          <div className='flexDiv' />
           <div>
-            <p style={{ marginRight: '20px', fontWeight: 'bold', color: 'black', marginLeft: '10px' }}>{user.username}</p>
-            <p style={{ margin: '0', fontSize: '14px', color: 'black' }}>{user.email}</p>
+            <input
+              type="text"
+              autoFocus
+              value={searchQuery}
+              onChange={handleChange}
+              placeholder="Enter account name or query here..."
+            />
+            {suggestions.length > 0 && searchQuery.length > 0 &&
+              <ul>
+                {suggestions.map(suggestion => (
+                  <li key={suggestion.AccountId} onClick={() => { setSelectedAccount(suggestion.AccountId); setAccounts([suggestion]); setSearchQuery(suggestion.AccountName); setSuggestions([]) }}>
+                    {suggestion.AccountName}
+                  </li>
+                ))}
+              </ul>}
           </div>
-        </div>
-      </header>
-
-      {accounts.length === 0 && <img className='bg-image' src={"logo_large.png"} />}
-      <h1 className='subject'>iReach</h1>
-      <form>
-        <div className='flexDiv' />
-        <div>
-          <input
-            type="text"
-            autoFocus
-            value={searchQuery}
-            onChange={handleChange}
-            placeholder="Enter account name or query here..."
-          />
-          {suggestions.length > 0 && searchQuery.length > 0 &&
-            <ul>
-              {suggestions.map(suggestion => (
-                <li key={suggestion.AccountId} onClick={() => { setSelectedAccount(suggestion.AccountId); setAccounts([suggestion]); setSearchQuery(suggestion.AccountName); setSuggestions([]) }}>
-                  {suggestion.AccountName}
-                </li>
-              ))}
-            </ul>}
-        </div>
-        <button className="submit-button" disabled={searchQuery.trim() === ''} onClick={(e) => handleSubmit(e, 'dynamicSearch')}>
-          Standard Search
-        </button>
-        <button className="submit-button" disabled={searchQuery.trim() === ''} onClick={(e) => handleSubmit(e, 'semanticSearch')}>
-          Semantic Search
-        </button>
-        <div className='flexDiv' />
-      </form>
-      <div className="accounts">
-        {accounts.map((account) => (
-          <div key={account.AccountId} className={"account" + (account.AccountId === selectedAccount && selectedAccount !== "" ? " selected" : "")} onClick={() => handleSelect(account.AccountId)}>
-            <h2 className='accountName'>{account.AccountName}</h2>
-            <h4 className='industry'>{account.Industry}</h4>
-            {account.AnnualRunRate > 0 && <h5><em>Estimated Annual Run Rate: {account.AnnualRunRate}</em></h5>}
-            <p className='usecaseDesc'>{account.UseCaseDescription}</p>
-          </div>
-        ))}
-      </div>
-      {isPromtTextBarVisible && (<div className='generate-email'>
-        <div className='text-area-email'>
-          <textarea className='prompt-text-area'
-            type="text"
-            autoFocus
-            value={promptText}
-            onChange={handlePromptChange}
-            placeholder="Enter email generation prompt here..."
-          />
-        </div>
-        <button className='generate-email-button' disabled={promptText.trim() === ''} onClick={(e) => generateEmail(e)}>
-          Generate Email <RiMailLine />
-        </button>
-
-        <div className='email-text'>
-          {emailText}
-
-          <button className='copy-text'
-            onClick={copyText}
-          >
-            Copy Email <FiCopy style={{ color: 'white', size: '1.5em' }} />
+          <button className="submit-button" disabled={searchQuery.trim() === ''} onClick={(e) => handleSubmit(e, 'dynamicSearch')}>
+            Standard Search
           </button>
+          <button className="submit-button" disabled={searchQuery.trim() === ''} onClick={(e) => handleSubmit(e, 'semanticSearch')}>
+            Semantic Search
+          </button>
+          <div className='flexDiv' />
+        </form>
+        <div className="accounts">
+          {accounts.map((account) => (
+            <div key={account.AccountId} className={"account" + (account.AccountId === selectedAccount && selectedAccount !== "" ? " selected" : "")} onClick={() => handleSelect(account.AccountId)}>
+              <h2 className='accountName'>{account.AccountName}</h2>
+              <h4 className='industry'>{account.Industry}</h4>
+              {account.AnnualRunRate > 0 && <h5><em>Estimated Annual Run Rate: ${account.AnnualRunRate}</em></h5>}
+              <h5>Contact: {`${account.ContactName} (${account.role})`}</h5>
+              <p className='usecaseDesc'>{account.UseCaseDescription}</p>
+            </div>
+          ))}
         </div>
-      </div>
-      )}
-      <div className='powered'>
-        <div className='flexDiv'></div>
-        <img height={24} src="/mongo.png"></img>
-        <a className='powered-text' href="https://www.mongodb.com/products/platform/atlas-vector-search"><em>Powered by MongoDB Atlas Vector Search</em></a>
-        <div className='flexDiv'></div>
+        {selectedAccount !== "" && (<div className='generate-email'>
+          <div className='text-area-email'>
+            <textarea className='prompt-text-area'
+              type="text"
+              autoFocus
+              value={promptText}
+              onChange={handlePromptChange}
+              placeholder="Enter email generation prompt here..."
+            />
+            <button className='generate-email-button' disabled={promptText.trim() === ''} onClick={(e) => generateEmail(e)}>
+              Generate Email <RiMailLine />
+            </button>
+          </div>
+          <div className='email-text'>
+            <textarea className='prompt-text-area'
+              type="text"
+              autoFocus
+              value={emailText}
+              onChange={handleEmailChange}
+            />
+
+            <button className='generate-email-button' onClick={copyText}>
+              Copy Email <FiCopy style={{ color: 'black', size: '1.5em' }} />
+            </button>
+          </div>
+        </div>
+        )}
+
+        <header className='header'>
+          <div style={{ marginLeft: 20, color: 'black', fontSize: 18 }}>
+            iReach
+          </div>
+          <div className='flexDiv' />
+          <div style={{ marginRight: 20, display: 'flex', color: 'black' }}>
+            <FiUser size="30px" color='black' className='userIcon' />
+            <div>
+              <p className='userName'>{user.username}</p>
+              <p className='userEmail'>{user.email}</p>
+            </div>
+          </div>
+        </header>
+
+
+        <div className='powered'>
+          <div className='flexDiv'></div>
+          <img height={24} src="/mongo.png"></img>
+          <a className='powered-text' href="https://www.mongodb.com/products/platform/atlas-vector-search"><em>Powered by MongoDB Atlas Vector Search</em></a>
+          <div className='flexDiv'></div>
+        </div>
       </div>
     </div>
   );
